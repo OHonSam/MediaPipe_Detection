791271cc3de8a88dcbd3b55c2d48e50da31b8b19
zHuskyz <nvk01052004@gmail.com>
2024-09-05 23:09:26 +0700

    k means

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import copy
-import itertools
 from flask import Flask, render_template, Response
 import cv2
 import mediapipe as mp
 import numpy as np
 from sklearn.cluster import KMeans
+import time
 
 app = Flask(__name__)
 
@@ -21,43 +20,15 @@ camera = cv2.VideoCapture(0)
 # Initialize frame counter
 frame_number = 0
 
-# Initialize lists to store hand and pose landmarks
-right_hand_landmarks_list = []
-left_hand_landmarks_list = []
-pose_landmarks_list = []
-hand_pose_landmarks = []
 
 @app.route('/')
 def index():
     # Render the homepage
     return render_template('index.html')
 
-def pre_process_landmark(landmark_list):
-    '''
-    Hàm preprocess landmark
-    '''
-    temp_landmark_list = copy.deepcopy(landmark_list)
-
-    base_x, base_y = 0, 0
-    for index, landmark_point in enumerate(temp_landmark_list):
-        if index == 0:
-            base_x, base_y = landmark_point[0], landmark_point[1]
-
-        temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
-        temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
-
-    temp_landmark_list = list(
-        itertools.chain.from_iterable(temp_landmark_list))
-
-    # Find the furthest point from the base points
-    max_value = max(list(map(abs, temp_landmark_list)))
-
-    def normalize_(n):
-        return n / max_value
-
-    temp_landmark_list = list(map(normalize_, temp_landmark_list))
-
-    return temp_landmark_list
+# Function to run KMeans
+def run_kmeans(X):
+    kmeans = KMeans(n_clusters=20, random_state=0).fit(X)
 
 dataForKmeans = []
 
@@ -88,8 +59,9 @@ def generate_frames():
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
             # Initialize placeholders for left and right hand landmarks
-            # left_hand_landmarks = [(0, 0, 0)] * 21  # 21 landmarks for left hand initialized to (0, 0, 0)
-            # right_hand_landmarks = [(0, 0, 0)] * 21  # 21 landmarks for right hand initialized to (0, 0, 0)
+            pose_landmarks_list = []
+            left_hand_landmarks = [(0, 0, 0)] * 21  # 21 landmarks for left hand initialized to (0, 0, 0)
+            right_hand_landmarks = [(0, 0, 0)] * 21  # 21 landmarks for right hand initialized to (0, 0, 0)
 
             # Draw hand landmarks if detected and print 3D coordinates
             if hand_results.multi_hand_landmarks:
@@ -102,26 +74,21 @@ def generate_frames():
                     # Print 3D hand landmark coordinates (x, y, z)
                     for idx, landmark in enumerate(hand_landmarks.landmark):
                         h, w, _ = image.shape
-                        hand_landmarks_list = [[float(landmark.x * w), float(landmark.y * h), landmark.z]
+                        hand_landmarks_list = [[np.float16(landmark.x * w), np.float16(landmark.y * h), landmark.z]
                                     for landmark in hand_landmarks.landmark]
                         
-                        # if hand_label == 'Left':
-                        #     right_hand_landmarks = hand_landmarks_list  # Assign to right hand landmarks
-                        # elif hand_label == 'Right':
-                        #     left_hand_landmarks = hand_landmarks_list  # Assign to left hand landmarks
+                        if hand_label == 'Left':
+                            right_hand_landmarks = hand_landmarks_list  # Assign to right hand landmarks
+                        elif hand_label == 'Right':
+                            left_hand_landmarks = hand_landmarks_list  # Assign to left hand landmarks
                     
-            
+
             # Print the left hand landmarks (or placeholders if no left hand detected)
             # print("Left hand landmarks:")
-                # pre_process_right_hand_landmark_list = pre_process_landmark(right_hand_landmarks)
-                # pre_process_left_hand_landmark_list = pre_process_landmark(left_hand_landmarks)
-                pre_process_hand_landmark_list = pre_process_landmark(hand_landmarks_list)
-                print(pre_process_hand_landmark_list)
-
             # for idx, (x, y, z) in enumerate(left_hand_landmarks):
             #     print(f"Landmark {idx}: (x: {x}, y: {y}, z: {z:.4f})")
 
-            #         # Print the right hand landmarks (or placeholders if no right hand detected)
+            # Print the right hand landmarks (or placeholders if no right hand detected)
             # print("Right hand landmarks:")
             # for idx, (x, y, z) in enumerate(right_hand_landmarks):
             #     print(f"Landmark {idx}: (x: {x}, y: {y}, z: {z:.4f})")
@@ -136,30 +103,29 @@ def generate_frames():
                 # Print 3D pose landmark coordinates (x, y, z)
                 for idx, landmark in enumerate(pose_results.pose_landmarks.landmark):
                     h, w, _ = image.shape
-                    cx, cy, cz = float(landmark.x * w), float(landmark.y * h), landmark.z
+                    cx, cy, cz = np.float16(landmark.x * w), np.float16(landmark.y * h), landmark.z
                     pose_landmarks_list = [[cx, cy, cz] for landmark in pose_results.pose_landmarks.landmark]
                     # print(f"Pose landmark {idx}: (x: {cx}, y: {cy}, z: {cz:.4f})")
             
-                pre_process_pose_landmark = pre_process_landmark(pose_landmarks_list)
-                print(pre_process_pose_landmark)
+            
+            hand_pose_landmarks = left_hand_landmarks + right_hand_landmarks + pose_landmarks_list
+            hand_pose_landmarks = np.array(hand_pose_landmarks)
+            hand_pose_landmarks = hand_pose_landmarks.flatten()
 
-            '''
-                Kết hợp right_hand_landmark, left_hand_landmark và pose_landmark nếu như có hand_landmark và pose_landmark được 1 data points
-            '''
-            if pose_results.pose_landmarks and hand_results.multi_hand_landmarks:
-                # hand_pose_landmarks = pre_process_right_hand_landmark_list + pre_process_left_hand_landmark_list + pre_process_pose_landmark
-                hand_pose_landmarks = pre_process_hand_landmark_list + pre_process_pose_landmark
 
-                dataForKmeans.append(hand_pose_landmarks)
+            dataForKmeans.append(hand_pose_landmarks)
 
-                if len(dataForKmeans) == 30:
-                    kmeans = KMeans(n_clusters=20, random_state=0).fit(dataForKmeans)
-                    print('Centers found by scikit-learn:')
-                    print(kmeans.cluster_centers_)
-                    for i in range(15):
-                        dataForKmeans.pop(0)
+            if len(dataForKmeans) == 20:
+                start_time = time.time()
+                kmeans = KMeans(n_clusters=15, random_state=0).fit(dataForKmeans)
+                end_time = time.time()
+                execution_time = end_time - start_time
+                print(f"Running time of KMeans: {execution_time:.4f} seconds")
+                print('Centers found by scikit-learn:')
+                print(kmeans.cluster_centers_)
+                for i in range(15):
+                    dataForKmeans.pop(0)
                     
-
 
             # Encode the frame to be sent to the client
             ret, buffer = cv2.imencode('.jpg', image)
